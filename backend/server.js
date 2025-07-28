a378dd608f92e2691e0bd84e3d460d2753487226
aPineappleDev <supdevpine@gmail.com>
2025-07-28 23:30:31 +0530

    organised code, adding some logging. chat updation and date broken.

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,6 @@ const mongodb = require('mongodb')
 const cors = require('cors')
 require('dotenv').config()
 
-
 const uri = process.env.URI;
 const client =  new mongodb.MongoClient(uri);
 const db = client.db("ChatApp");
@@ -25,56 +24,143 @@ const io = new sio.Server(server, {
 
 
 
+io.on('connection', (socket) => {
 
-io.on('connection', async(socket) => {
-  console.log("connected.");
+  console.info("New Connection: Hello, " + socket.handshake.headers['username'] + "!");
 
-  socket.on("sendMessage", async(socket) => {
-    console.log("recieving and sending message to " + socket.roomCode);
-    console.log("message data: " + JSON.stringify(socket.messageData));
-    let filter  = {roomCode : socket.roomCode};
-    let chatRoom = await chatRooms.findOne(filter);
-    let messageNum = (chatRoom["data"]["messageCount"] + 1);
-    console.log("updating...")
-    let update = {
-      $set : {
-        data : {
-          ...chatRoom["data"],
-          messageCount : messageNum,
-          history : {
-            ...chatRoom["data"]["history"],
-            [messageNum.toString()] : socket["messageData"]
-          }
-        }
-      }
-    }
-    let result = await chatRooms.updateOne(filter, update, {upsert : true})
-    console.log("successfully updated")
-    chatRoom = await chatRooms.findOne(filter);
-    console.log(typeof chatRoom["data"]["history"])
-    let messages = [];
-    for (let i = 1; i <= chatRoom["data"]["messageCount"]; i++ ) {
-      console.log(i + " : " + JSON.stringify(chatRoom["data"]["history"][i.toString()]));
-      let sender = chatRoom["data"]["history"][i.toString()]["sender"];
-      let d = new Date()
-      let sendTime = chatRoom["data"]["history"][i.toString()]["date"];
-      d.setSeconds(sendTime);
-      let formattedTime = d.getDate() + "/" + d.getMonth()+1 + "/" + d.getFullYear() + " "
-                        + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
-      let message = chatRoom["data"]["history"][i.toString()]["message"];
+  console.debug("Adding user to their rooms...")
+  addClientToRooms(socket);
+  console.debug("Done!\n")
 
-      messages.push(`[${ sender }][${ formattedTime }]:${ message }`);
-    }
-    console.log(messages);
-    io.to(socket.roomCode).emit("updateMessages", {
-      roomCode : socket.roomCode,
-      messageData : messages
-    });
-  })
+  socket.on("sendMessage", async (data) => await sendMessage(data));
+  // socket.on("createRoom", async(data) => await createRoom(data));
+  // socket.on("leaveRoom", async(data) => await leaveRoom(data));
+  // socket.on("deleteRoom", async(data) => await deleteRoom(data));
 
-
-  socket.join("global")
 })
+
+
+function addClientToRooms(socket) {
+  socket.join("global");
+}
+
+
+async function sendMessage(data) {
+
+  console.info("Sending\n" + data['messageData']['message'] +
+    "\n to chat room [code]\n" + data['roomCode']);
+
+  let filter = { roomCode: data['roomCode'] };
+  console.debug("Finding room with room code: " + data['roomCode'] + "...");
+  let chatRoom = await chatRooms.findOne(filter);
+  console.debug("Found!\n");
+
+  let newMessageNum = chatRoom['data']['messageCount'] + 1;
+  chatRoom['data']['messageCount'] = newMessageNum;
+  chatRoom['data']['history'][newMessageNum.toString()] = data['messageData'];
+
+  let update = {
+    $set: chatRoom
+  }
+  console.debug("Updating messages in room [" + data['roomCode'] + "]...");
+  let updateResult = await chatRooms.updateOne(filter, update, { upsert: true });
+  console.debug("Updated!\n");
+
+  updateClientMessages(chatRoom);
+}
+
+
+function updateClientMessages (chatRoom) {
+
+  let chatHistory = {};
+
+  console.debug("Creating readable and formatted list of messages....")
+  // set end to the final message number
+  let end = chatRoom['data']['messageCount'];
+  // set start to 50th message from the end
+  // !!!!TODO (ADD OPTION IN CLIENT SIDE TO LOAD MORE MESSAGES)!!!!
+  let start = (end - 50 > 0) ? end - 50 : 1;
+  for (let i = start; i <= end; i++) {
+    let msgData = chatRoom['data']['history'][i.toString()];
+    let date = new Date();
+    date.setUTCMilliseconds(msgData['epochTime']);
+    let formattedDateString = date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear();
+    let formattedTimeString = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+    let msgFormattedStringHTML = `[${ formattedDateString }][${ formattedTimeString }][${ msgData['sender'] }] :<br>`
+                              + msgData['message'];
+    chatHistory[i.toString()] = msgFormattedStringHTML;
+  }
+  console.debug("Done!\n")
+
+  let data = {
+    roomCode : chatRoom['roomCode'],
+    msgStartNum : start,
+    msgEndNum : end,
+    history : chatHistory
+  }
+
+  console.debug("Sending updated data to all room members...\n")
+  io.to(chatRoom['roomCode']).emit("updateChat", data);
+}
+
+
+
+
+
+
+
+
+
+
+// io.on('connection', async(socket) => {
+//   console.log("connected.");
+
+//   socket.on("sendMessage", async(socket) => {
+//     console.log("recieving and sending message to " + socket.roomCode);
+//     console.log("message data: " + JSON.stringify(socket.messageData));
+//     let filter  = {roomCode : socket.roomCode};
+//     let chatRoom = await chatRooms.findOne(filter);
+//     let messageNum = (chatRoom["data"]["messageCount"] + 1);
+//     console.log("updating...")
+//     let update = {
+//       $set : {
+//         data : {
+//           ...chatRoom["data"],
+//           messageCount : messageNum,
+//           history : {
+//             ...chatRoom["data"]["history"],
+//             [messageNum.toString()] : socket["messageData"]
+//           }
+//         }
+//       }
+//     }
+//     let result = await chatRooms.updateOne(filter, update, {upsert : true})
+//     console.log("successfully updated")
+//     chatRoom = await chatRooms.findOne(filter);
+//     console.log(typeof chatRoom["data"]["history"])
+//     let messages = [];
+//     for (let i = 1; i <= chatRoom["data"]["messageCount"]; i++ ) {
+//       console.log(i + " : " + JSON.stringify(chatRoom["data"]["history"][i.toString()]));
+//       let sender = chatRoom["data"]["history"][i.toString()]["sender"];
+//       let d = new Date()
+//       let sendTime = chatRoom["data"]["history"][i.toString()]["date"];
+//       d.setSeconds(sendTime);
+//       let formattedTime = d.getDate() + "/" + d.getMonth()+1 + "/" + d.getFullYear() + " "
+//                         + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
+//       let message = chatRoom["data"]["history"][i.toString()]["message"];
+
+//       messages.push(`[${ sender }][${ formattedTime }]:${ message }`);
+//     }
+//     console.log(messages);
+//     io.to(socket.roomCode).emit("updateMessages", {
+//       roomCode : socket.roomCode,
+//       messageData : messages
+//     });
+//   })
+
+
+//   socket.join("global")
+// })
 
 
 
--- frontend/index.js
+++ frontend/index.js
@@ -1,49 +1,121 @@
 import { io } from "socket.io-client";
 
-const port = 7845
-const socket = io(`http://localhost:${port}`)
+localStorage.setItem('username', 'pineapple')
 
-let chatHistories = {
-  "global" : []
-};
+const port = 7845;
+console.debug("Connecting to web socket...");
+const socket = io(`http://localhost:${port}`, {
+  extraHeaders : {
+    username : localStorage.getItem('username')
+  }
+});
+console.debug("Connected!");
 
-document.addEventListener("DOMContentLoaded", () => {
-  let historyDiv = document.getElementById("history");
-  historyDiv.innerHTML = "hello";
-  socket.on("message", (msg) => {
-      console.log("recieved: " + msg);
-      historyDiv.innerHTML = historyDiv.innerHTML + "<br>" + msg;
-    })
+let chatHistories = {};
 
-  document.getElementById("sendMessageForm").addEventListener("submit", (e) => {
-    e.preventDefault();
-    let msg = document.getElementById("messageInput").value;
-    console.log("sending " + msg);
-    let payload = {
-      roomCode : "global",
-      messageData : {
-      sender : socket.id,
-      date : Date.now(),
-      message : msg
-      }
-    };
-    console.log("payload: " + JSON.stringify(payload))
-    socket.emit("sendMessage", payload)
-  })
+const historyDiv = document.getElementById('historyDiv');
+const chatRoomDiv = document.getElementById('chatRoomDiv');
+const sendMessageForm = document.getElementById('sendMessageForm');
+const messageInput = document.getElementById('messageInput');
 
-  socket.on("test", (data) => {
-    console.log(data);
-  })
-    // historyDiv.innerHTML = historyDiv.innerHTML + "<br>" + "test";
-    // console.log(historyDiv.innerHTML)
-    // return false;
-  socket.on("updateMessages", (data) => {
-    historyDiv.innerHTML = "";
-    chatHistories[data["roomCode"]] = data["messageData"];
-    console.log(chatHistories[data["roomCode"]]);
-    for (const msg of chatHistories[data["roomCode"]]) {
-      historyDiv.innerHTML += msg + "<br>";
+
+
+sendMessageForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  sendMessage()
+});
+
+
+socket.on("updateChat", (data) => updateHistoryData(data));
+
+
+function sendMessage() {
+
+   // let chatRoom = selectedChatRoom
+  let chatRoom = "global";
+  let sender = localStorage.getItem('username');
+  let message = messageInput.value;
+  let epochTime = Date.now();
+
+  let data = {
+    roomCode : chatRoom,
+    messageData : {
+      sender : sender,
+      epochTime : epochTime,
+      message : message
     }
+  }
 
-  })
-})
+  console.log("Sending Message...")
+  socket.emit('sendMessage', data);
+}
+
+
+function updateHistoryData(data) {
+  console.debug("Recieved update request. Updating chat history data...");
+  chatHistories[data['roomCode']] = data['history'];
+  chatHistories[data['roomCode']]['msgStartNum'] = data['msgStartNum'];
+  chatHistories[data['roomCode']]['msgEndNum'] = data['msgEndNum'];
+  console.debug("Done!\n")
+
+  // if (selectedChatRoom = data['roomCode']) updateChat();
+  updateChat();
+}
+
+
+function updateChat() {
+  console.debug("Updating current chat...");
+
+  // let roomCode = selectedChatRoom;
+  let roomCode = "global";
+  // let lastUpdatedMessage = getLastUpdatedMessage;
+  let lastUpdatedMessageNum = chatHistories[roomCode]['msgStartNum'];
+  let lastMessageInHistoryNum = chatHistories[roomCode]['msgEndNum'];
+  for (let i = lastUpdatedMessageNum; i <= lastMessageInHistoryNum; i++) {
+    historyDiv.innerHTML = historyDiv.innerHTML + chatHistories[roomCode][i.toString()] + "<br>";
+  }
+
+  console.debug("Done!\n")
+}
+
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   let historyDiv = document.getElementById("history");
+//   historyDiv.innerHTML = "hello";
+//   socket.on("message", (msg) => {
+//       console.log("recieved: " + msg);
+//       historyDiv.innerHTML = historyDiv.innerHTML + "<br>" + msg;
+//     })
+
+//   document.getElementById("sendMessageForm").addEventListener("submit", (e) => {
+//     e.preventDefault();
+//     let msg = document.getElementById("messageInput").value;
+//     console.log("sending " + msg);
+//     let payload = {
+//       roomCode : "global",
+//       messageData : {
+//       sender : socket.id,
+//       date : Date.now(),
+//       message : msg
+//       }
+//     };
+//     console.log("payload: " + JSON.stringify(payload))
+//     socket.emit("sendMessage", payload)
+//   })
+
+//   socket.on("test", (data) => {
+//     console.log(data);
+//   })
+//     // historyDiv.innerHTML = historyDiv.innerHTML + "<br>" + "test";
+//     // console.log(historyDiv.innerHTML)
+//     // return false;
+//   socket.on("updateMessages", (data) => {
+//     historyDiv.innerHTML = "";
+//     chatHistories[data["roomCode"]] = data["messageData"];
+//     console.log(chatHistories[data["roomCode"]]);
+//     for (const msg of chatHistories[data["roomCode"]]) {
+//       historyDiv.innerHTML += msg + "<br>";
+//     }
+
+//   })
+// })
